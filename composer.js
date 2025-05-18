2be376c1542372b87bf85d0b95e0fbc18aecde2a
krytec_gaming <95090991+ClosetCreeper@users.noreply.github.com>
2025-05-18 13:44:51 -0600

    Update composer.js
    Changed to REACT keys instead of raw data.

--- composer.js
+++ composer.js
@@ -15,12 +15,13 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAwRDBygJi-lzmxm-Pm4tHUHOQQxwdd9YE',
-  authDomain: 'jcaccollaboration.firebaseapp.com',
-  projectId: 'jcaccollaboration',
-  storageBucket: 'jcaccollaboration.firebasestorage.app',
-  messagingSenderId: '1011711048647',
-  appId: '1:1011711048647:web:1c4696f4852a9993b237e2',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
